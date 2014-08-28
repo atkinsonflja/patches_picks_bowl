b953df2665d89310c4dd789be7e0730e9cf3345c
atkinsonflja <atkinsonflja@gmail.com>
2014-08-28 02:03:18 -0400

    Last Minute Changes

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,7 +1,5 @@
-setTimeout(function () {
-	$( document ).ready(function() {
-	  $(".vote input[type=radio]").on("click", function() {
-	    $("#maintitle").text($(this).data("tt"));
-	  });
-	});
-}, 1000);
+$( document ).ready(function() {
+  $(".vote input[type=radio]").on("click", function() {
+    $("#maintitle").text($(this).data("tt"));
+  });
+});
