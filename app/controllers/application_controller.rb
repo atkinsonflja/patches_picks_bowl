ba1ec40dac0e5c018fcde8c9f4b4d2a04d3bb485
atkinsonflja <atkinsonflja@gmail.com>
2014-08-28 00:29:40 -0400

    Last Minute Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   @hostname = request.host || "www.patchespicks.herokuapp.com"
-
-  if @hostname == "www.patchespicks.herokuapp.com"
-  	redirect_to "picks.taxslayerbowl.com", status: 301
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
 
   # GET /
   def index
-  	redirect_to 'picks.taxslayerbowl.com', :status => :moved_permanently
   end
 
   # GET /rules
