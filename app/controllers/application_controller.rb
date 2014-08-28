316c1aec8b52e469640e7210b2a5cfb76dae8186
atkinsonflja <atkinsonflja@gmail.com>
2014-08-28 00:15:29 -0400

    Last Minute Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  @hostname = request.host || "www.patchespicks.herokuapp.com"
+
+  if @hostname == "www.patchespicks.herokuapp.com"
+  	redirect_to "picks.taxslayerbowl.com", status: 301
+  end
 end
